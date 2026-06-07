4b0f840aaa0fe4b6c627a0538a6350902307dcfd
msaadirfan <msaadirfan04@gmail.com>
2026-06-07 22:24:53 +0500

    preparing files for frontend deployment

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './auth.css';
+import './Auth.css';
 
 const API_BASE = 'http://localhost:8000';
 
